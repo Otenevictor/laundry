05345884129220d27087e914255d1d3f39c0b26d
Otene Victor <solomonotene36@gmail.com>
2022-11-01 23:06:53 +0100

    first release

---
+++ html/script.js
@@ -0,0 +1,25 @@
+// window.prompt("Welcome to laundry");
+var modal = document.getElementById
+('id01');
+
+window.onclick = function(event) {
+    If (event.target == modal) 
+    {
+     modal.style.display = "none";
+    }
+}
+
+
+
+
+// navbar function
+
+// function openNav() {
+//     document.getElementById("myNav")
+//     .style.width ="100%";
+//    }
+   
+//    function closeNav() {
+//     document.getElementById("myNav")
+//     .style.width = "0%";
+//    }
